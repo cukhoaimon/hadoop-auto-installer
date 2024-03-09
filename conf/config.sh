1f5ecc2d9130d9ce2e86f09b8bfcc20768bab0c4
Queensalats <60815035+cukhoaimon@users.noreply.github.com>
2024-03-09 11:54:21 +0700

    Update config.sh

--- conf/config.sh
+++ conf/config.sh
@@ -1,8 +1,10 @@
 # change your user password as root
 HADOOP_USER_PASSWORD=1213
+
 # please change cukhoaimon to your username
 HADOOP_PARENT_DIR=/home/cukhoaimon/hadoop
 
+# url to download hadoop binaries version 3.3.6
 HADOOP_ORIGIN=https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz
 
 # if you install different java version, please change here
